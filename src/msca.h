602219e5b77d5e22e27cb31be193a78021508a79
Rongcui Dong <rongcuid@outlook.com>
2024-01-16 22:02:22 -0500

    Spurious include

--- src/msca.h
+++ src/msca.h
@@ -4,7 +4,6 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <sys/_types/_null.h>
 
 // The following is the canonical definition of arrow C data interface
 // structures
