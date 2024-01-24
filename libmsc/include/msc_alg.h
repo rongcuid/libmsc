bd61cc2a80df1a6463829d3bc921750312f08811
Rongcui Dong <rongcuid@outlook.com>
2024-01-24 13:40:25 -0500

    Static library

--- libmsc/include/msc_alg.h
+++ libmsc/include/msc_alg.h
@@ -4,10 +4,30 @@
 #include <stddef.h>
 #include <stdint.h>
 
+/**
+ * @brief Swaps two segments of memory. Allocates `size` amount of temporary
+ * buffer.
+ */
+void mscalg_swap(void *a, void *b, size_t size);
+
+/**
+ * @brief Comparison function for sorts.
+ *
+ * @param context Arbitrary user pointer
+ * @return If a < b, less than 0; if equal, return 0; otherwise, greater than 0
+ */
 typedef ptrdiff_t (*msc_cmp_f)(const void *context, const void *a,
                                const void *b);
 
-void mscalg_swap(void *a, void *b, size_t size);
+/**
+ * @brief Use quick sort to sort `items`
+ *
+ * @param items Pointer to array
+ * @param len Length of array
+ * @param elem_size Size of each element
+ * @param comp Comparison function
+ * @param context Passed as first argument to comparison function
+ */
 void mscalg_qsort(void *items, size_t len, size_t elem_size, msc_cmp_f comp,
                   void *context);
 
---
+++ libmsc/include/msc_alloc.h
@@ -0,0 +1,4 @@
+#ifndef MSC_ALLOC_H_
+#define MSC_ALLOC_H_
+
+#endif  // MSC_ALLOC_H_
