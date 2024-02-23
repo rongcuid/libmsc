defed3998d5ae2e7543dda8871e1feb16787a668
Rongcui Dong <rongcuid@outlook.com>
2024-02-23 11:47:22 -0500

    Adding scratchpad

--- src/renderer/renderer.c
+++ src/renderer/renderer.c
@@ -1,8 +1,8 @@
 #include "renderer.h"
 
 #include <SDL3/SDL_vulkan.h>
+#include <msc_arena.h>
 #include <volk.h>
-#include <vulkan/vulkan_core.h>
 
 #include "vk/device.h"
 #include "vk/instance.h"
@@ -12,17 +12,25 @@
 ////// Renderer
 
 struct Renderer_T {
+  void *scratch_memory;
+  struct msc_arena scratch;
   struct Instance instance;
   VkSurfaceKHR surface;
   struct Device device;
 };
 
+static const size_t SCRATCH_SIZE = 16 * 1024 * 1024;
+
 bool createRenderer(Renderer *pRenderer, bool validate, SDL_Window *window) {
   SDL_Log("Creating renderer");
   bool ok = false;
+  //
   Renderer renderer = SDL_malloc(sizeof(struct Renderer_T));
   if (!renderer) goto finally;
-  if (!initInstance(&renderer->instance, validate)) goto finally;
+  renderer->scratch_memory = SDL_malloc(SCRATCH_SIZE);
+  if (!renderer->scratch_memory) goto clean_renderer;
+  msc_arena_init(&renderer->scratch, renderer->scratch_memory, SCRATCH_SIZE);
+  if (!initInstance(&renderer->instance, validate)) goto clean_scratch;
   if (!SDL_Vulkan_CreateSurface(window, renderer->instance.instance, NULL,
                                 &renderer->surface)) {
     goto clean_instance;
@@ -46,7 +54,9 @@ clean_instance:
                                       renderer->instance.messenger, NULL);
     vkDestroyInstance(renderer->instance.instance, NULL);
   }
-clean_malloc:
+clean_scratch:
+  if (!ok) SDL_free(renderer->scratch_memory);
+clean_renderer:
   if (!ok) SDL_free(renderer);
 finally:
   return ok;
@@ -57,5 +67,6 @@ void destroyRenderer(Renderer renderer) {
   deinitDevice(&renderer->device);
   vkDestroySurfaceKHR(renderer->instance.instance, renderer->surface, NULL);
   deinitInstance(&renderer->instance);
+  SDL_free(renderer->scratch_memory);
   SDL_free(renderer);
 }
